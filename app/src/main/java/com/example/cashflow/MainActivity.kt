509b8882125816a1e4f4f766515f99bf975a4394
HaTungAnh <21021453@vnu.edu.vn>
2024-04-04 20:32:26 +0700

    feat:Spending & Earned Tabs Initial

--- app/src/main/java/com/example/cashflow/MainActivity.kt
+++ app/src/main/java/com/example/cashflow/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cashflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TabHost
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabHost = findViewById<TabHost>(R.id.tabHost)
+        tabHost.setup()
+
+        var spec: TabHost.TabSpec = tabHost.newTabSpec("Tab One")
+        spec.setContent(R.id.tab1)
+        spec.setIndicator("Tiền chi tiêu")
+        tabHost.addTab(spec)
+
+        spec = tabHost.newTabSpec("Tab Two")
+        spec.setContent(R.id.tab2)
+        spec.setIndicator("Tiền kiếm được")
+        tabHost.addTab(spec)
     }
 }
